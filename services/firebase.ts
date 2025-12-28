47eb0861000251bc3eb7ce9121afb1d9bad78706
mohcineelhanoune <152867482+mohcineelhanoune@users.noreply.github.com>
2025-12-28 17:53:38 +0100

    Remove Firebase config from firebase.ts
    Removed sensitive Firebase configuration details.

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,12 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeDSEtl2g4MDJjrXpDbtRE_jc1feQwNlM",
-  authDomain: "shopcard-76189.firebaseapp.com",
-  projectId: "shopcard-76189",
-  storageBucket: "shopcard-76189.firebasestorage.app",
-  messagingSenderId: "64193230928",
-  appId: "1:64193230928:web:72c13767870a37a82bb955"
+
 };
 
 const app = initializeApp(firebaseConfig);
